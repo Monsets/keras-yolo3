0aa2685e23396f6983c3d85da35121c121ed5a9d
monset <monset008@gmail.com>
2019-05-04 13:38:59 +0300

    faces support

--- train.py
+++ train.py
@@ -16,7 +16,7 @@ from yolo3.utils import get_random_data
 def _main():
     annotation_path = 'attributes.txt'
     log_dir = 'logs/000/'
-    classes_path = 'model_data/face_classes'
+    classes_path = 'model_data/face_classes.txt'
     anchors_path = 'model_data/yolo_anchors.txt'
     class_names = get_classes(classes_path)
     num_classes = len(class_names)
